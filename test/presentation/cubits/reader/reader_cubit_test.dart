a57adaa0364e267846a521d58fa281b6870e23c7
Asix <khoirul.20998@gmail.com>
2026-02-22 13:46:30 +0700

    chore: Add ignore directive for depend_on_referenced_packages in reader_cubit_test.dart

--- test/presentation/cubits/reader/reader_cubit_test.dart
+++ test/presentation/cubits/reader/reader_cubit_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:mocktail/mocktail.dart';
