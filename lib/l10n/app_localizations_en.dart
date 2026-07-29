21d3f41b56377c8859a7b5b23f3f4212bba5b1c6
Asix <khoirul.20998@gmail.com>
2026-07-29 21:21:55 +0700

    Update localization comments to use documentation style for clarity
    - Changed comments in app_localizations_en.dart, app_localizations_id.dart, and app_localizations_zh.dart
    - Updated from single-line comments to documentation comments for better readability and consistency

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -4,7 +4,7 @@ import 'app_localizations.dart';
 
 // ignore_for_file: type=lint
 
-// The translations for English (`en`).
+/// The translations for English (`en`).
 class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
